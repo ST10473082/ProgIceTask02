47a0a2e2e782aeed8dc3fa068af1f34ba247777e
ST10473082 <ST10473082@MyEmeris.edu.za>
2026-04-22 11:56:15 +0200

    blood types

---
+++ ProgIceTask02/src/main/java/com/mycompany/progicetask02/BloodData.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progicetask02;
+
+/**
+ *
+ * @author moran
+ */
+public class BloodData {
+    private String bloodType;
+    private String rhFactor;
+    
+    public BloodData() { 
+        bloodType = "O";
+        rhFactor = "+";
+    }
+    
+    public BloodData(String bloodType, String rhFactor) {
+        this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
+    }
+    
+    public String getBloodType() {
+        return bloodType;
+    }
+    
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+    
+    public String getrhFactor() { 
+        return rhFactor;
+    }
+}
